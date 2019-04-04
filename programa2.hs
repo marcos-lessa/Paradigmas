ed572fe86aab66d83afb0813b29d550a16016e26
Marcos Lessa <marcos-lessa@outlook.com>
2019-04-04 09:49:24 -0300

    atividade6

---
+++ programa2.hs
@@ -0,0 +1,14 @@
+data Forma = Circulo Float | Retangulo Float Float | Triangulo Float Float
+area :: Forma -> Float
+area (Circulo r) = pi * r * r
+area (Retangulo b a) = b * a
+area (Triangulo a b) = a*b/2
+
+minhaForma :: Forma
+minhaForma = (Retangulo 4 6)
+
+main = do 
+	putStrLn ( show (area (Circulo 6.42)) )
+	putStrLn ( show (area (Retangulo 4 5)) ) 
+	putStrLn ( show (area minhaForma) )
+	putStrLn (show (area (Triangulo 10 5)))
